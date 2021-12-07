5ff385b5d4ab5653592b604b0a154b116e6c6257
John Winans <john@winans.org>
2021-12-07 15:49:56 -0600

    Add A operand (official instruction notation)

--- hello/hello.asm
+++ hello/hello.asm
@@ -3,7 +3,7 @@
     ld      a,0xab     ; load the accumulator with the hex value AB
     ld      b,0x10     ; load the B register with the hex value 10
 loop:                  ; a label used in a jump instruction below
-    add     b          ; A = A + B
+    add     a,b        ; A = A + B
     jp      loop       ; load the PC with the address represented by 'loop'
 
     ds      0x100,0    ; reserve 256 bytes of space and initialize them all to zero 
